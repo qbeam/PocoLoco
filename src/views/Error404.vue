0bcdcccd37d7c544718cf69f720cc5ee5456e659
yingspvd <supavadeeying@outlook.com>
2021-06-04 05:35:21 +0700

    Error404

---
+++ src/views/Error404.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <div class="image">
+      <img src="../assets/error.png" />
+    </div>
+    <h1>Sorry, but this page doesn’t exist.</h1>
+    <p>Maybe you would like to go back to our <a href="/"> Login</a></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Error404",
+};
+</script>
+
+<style scoped>
+.container {
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+img {
+  width: 200px;
+  align-self: center;
+  margin-bottom: 20px;
+}
+p {
+  margin-top: 0;
+}
+</style>
